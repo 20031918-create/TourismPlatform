fbf286faed7a155f6e979e083a060d7012c0d189
20030857-arch <20030857@students.koi.edu.au>
2026-01-21 10:54:12 +1100

    Added Destinations page + navbar link

---
+++ TourismPlatform/Controllers/DestinationsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace TourismPlatform.Controllers
+{
+    public class DestinationsController : Controller
+    {
+        // GET: Destinations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Destinations/Details?name=Newcastle
+        public ActionResult Details(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
