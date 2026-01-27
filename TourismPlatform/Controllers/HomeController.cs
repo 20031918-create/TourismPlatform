293a079fa6e84666cefcdfb24135b06d869e3663
Prashant0309 <20032237@students.koi.edu.au>
2026-01-27 17:20:04 +1100

    Integrated Home page with Tourpackage browsing and featured tours.

--- TourismPlatform/Controllers/HomeController.cs
+++ TourismPlatform/Controllers/HomeController.cs
@@ -1,11 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using TourismPlatform.Models;
 
 namespace TourismPlatform.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            // Featured packages = active, available, and upcoming tours
+            var featured = db.TourPackages
+                .Include(t => t.TravelAgency)
+                .Where(t => t.IsActive && t.AvailableSlots > 0 && t.StartDate >= DateTime.Today)
+                .OrderByDescending(t => t.CreatedDate)
+                .Take(6)
+                .ToList();
+
+            // Fallback in case your test data has past StartDates
+            if (!featured.Any())
+            {
+                featured = db.TourPackages
+                    .Include(t => t.TravelAgency)
+                    .Where(t => t.IsActive && t.AvailableSlots > 0)
+                    .OrderByDescending(t => t.CreatedDate)
+                    .Take(6)
+                    .ToList();
+            }
+
+            ViewBag.FeaturedPackages = featured;
+
             return View();
         }
 
@@ -19,17 +46,24 @@ namespace TourismPlatform.Controllers
             return View();
         }
 
-       
         public ActionResult Review()
         {
             return View();
         }
 
-        
         [Authorize]
         public ActionResult Dashboard()
         {
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
